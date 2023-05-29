5617da299c69bc2754c4dff3a3eba1ca95bc568a
dennis-adrian <dennisguzmanbo@gmail.com>
2023-05-28 22:35:55 -0300

    Create folders by a given list of names

--- create_folder_inside_another.py
+++ create_folder_inside_another.py
@@ -10,25 +10,14 @@ from google.auth.transport.requests import Request
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
+folder_names = [
+    'name1',
+    'name2',
+    'name3'
+]
 
 
 def get_creds():
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-
-def create_folder():
     creds = None
 
     if os.path.exists('token.json'):
@@ -45,20 +34,31 @@ def create_folder():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+    return creds
+
+
+def create_folder():
+    creds = get_creds()
+
     try:
         # create drive api client
         service = build('drive', 'v3', credentials=creds)
-        file_metadata = {
-            'name': 'Invoices',
-            'mimeType': 'application/vnd.google-apps.folder',
-            'parents': ['1I97pGPORbIjFwojA9Oddv5fvyLbW8jKr']
-        }
 
-        # pylint: disable=maybe-no-member
-        file = service.files().create(body=file_metadata, fields='id'
-                                      ).execute()
-        print(F'Folder ID: "{file.get("id")}".')
-        return file.get('id')
+        folder_ids = []
+        for folder_name in folder_names:
+          file_metadata = {
+              'name': folder_name,
+              'mimeType': 'application/vnd.google-apps.folder',
+              'parents': ['1I97pGPORbIjFwojA9Oddv5fvyLbW8jKr']
+          }
+
+          # pylint: disable=maybe-no-member
+          file = service.files().create(body=file_metadata, fields='id'
+                                        ).execute()
+          print(F'Folder ID: "{file.get("id")}".')
+          folder_ids.append(file.get('id'))
+
+        return folder_ids
 
     except HttpError as error:
         print(F'An error occurred: {error}')
